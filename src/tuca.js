117e32b3e6eab0b44c0b878635b7e4800ad3e9b2
Liam <jlhs1001@gmail.com>
2022-08-29 17:11:59 -0400

    make collision const, create elastic function.

--- src/tuca.js
+++ src/tuca.js
@@ -1,3 +1,8 @@
-let Collision = {
-
+const Collision = {
+    // Elastic collisions refer to the simple cast where
+    // two entities collide and a transfer of energy is
+    // performed
+    elastic: (restitution) => {
+        this.restitution = restitution || .2;
+    }
 };
